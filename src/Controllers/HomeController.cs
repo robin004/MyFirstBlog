3a2ea43179d0d222bd2e0f99e8975073b5deb966
laptop <laptop@W4DEUMSY9001643.mms-t-systems.de>
2018-11-30 15:15:25 +0100

    first try

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Blog.Models;
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            ViewData["Message"] = "";
+
+            return View();
+        }
+
+        public IActionResult Test2()
+        {
+            ViewData["Message"] = "";
+
+            return View();
+        }
+
+        public IActionResult Kontakt()
+        {
+            ViewData["Message"] = "";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/readme.md
@@ -0,0 +1,5 @@
+# Blog
+
+## Know Issues
+
+- Aktuell funktionieren keine Markdown Tabellen.
